83ec0a2fd07440ed1e0972e20c0575d2dcdeacf2
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-12-02 11:12:17 +0100

    feat: added nullable

--- database/migrations/2025_11_11_234259_create_profiles_table.php
+++ database/migrations/2025_11_11_234259_create_profiles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained()->cascadeOnDelete();
             $table->string('avatar')->nullable();
-            $table->enum('userType', ['individual', 'business', 'household']);
+            $table->enum('userType', ['individual', 'business', 'household'])->nullable();
             $table->string('pickupLocation')->nullable();
             $table->timestamps();
         });
